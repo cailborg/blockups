cc6cb1d3b7a2b2f67c2a08b9f0db90ab56f5fb87
Cail Borg <Cail.Borg@ofx.com>
2020-07-10 14:34:15 +1000

    fixed typing issue locally

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import { color } from "styled-system";
 
+interface StyleProps {
+  bg?: string;
+}
+
 interface TestComponentProps {
   text: string;
 }
 
-const StyledDiv = styled.div`
+const StyledDiv = styled.div<StyleProps>`
   ${color}
   border: 1px solid black;
   padding: 16px;
@@ -21,7 +25,7 @@ const StyledHeading = styled.h1`
 const StyledDescription = styled.h2``;
 
 const TestComponent: React.FC<TestComponentProps> = ({ text }) => (
-  <StyledDiv data-testid="test-component" color="black">
+  <StyledDiv data-testid="test-component" color="black" bg="blue">
     <StyledHeading className="heading">I'm the test component</StyledHeading>
     <StyledDescription>{text}</StyledDescription>
   </StyledDiv>
