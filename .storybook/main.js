d6a762ae3bc1ec58a1751077ebd74212ad8cf120
Cail Borg <Cail.Borg@ofx.com>
2020-07-13 15:52:04 +1000

    add storybook docs and render color palette

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,7 @@
-const path = require("path");
-
 module.exports = {
-  stories: ["../src/**/*.stories.tsx"],
+  stories: ["../src/**/*.stories.*"],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [],
+  addons: ["@storybook/addon-docs"],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
